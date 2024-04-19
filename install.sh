eba74559265410a643f88e673278e5aa657db44c
MrKeith99 <kvalentincardenas@hotmail.com>
2024-04-19 14:54:43 +0900

    [UPD] update dependencies

--- install.sh
+++ install.sh
@@ -1,10 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "╔══╣ Install: ssd_nodelet (STARTING) ╠══╗"
 
 
-sudo apt-get update
+# Download SOBITS Msgs
+DIR=$(pwd)
+cd ..
+git clone https://github.com/TeamSOBITS/sobits_msgs.git
+cd $DIR
 
+
+# Install ROS Packages
+sudo apt-get update
+sudo apt-get install -y \
+    ros-${ROS_DISTRO}-cv-bridge \
+    ros-${ROS_DISTRO}-geometry-msgs \
+    ros-${ROS_DISTRO}-message-filters \
+    ros-${ROS_DISTRO}-pcl-ros \
+    ros-${ROS_DISTRO}-pcl-conversions \
+    ros-${ROS_DISTRO}-tf2 \
+    ros-${ROS_DISTRO}-tf2-ros \
+    ros-${ROS_DISTRO}-tf2-geometry-msgs \
+    ros-${ROS_DISTRO}-sensor-msgs \
+    ros-${ROS_DISTRO}-std-msgs \
+    ros-${ROS_DISTRO}-pluginlib \
+    ros-${ROS_DISTRO}-nodelet
+
+
+# Install Camera Packages
 sudo apt-get install -y \
     v4l-utils 
 
@@ -13,10 +36,12 @@ sudo apt-get install -y \
     ros-${ROS_DISTRO}-camera-calibration \
     ros-${ROS_DISTRO}-image-proc
 
-# Setting dynamixel USB1 (SOBIT PRO arm_pantilt)
+
+# Setting uvc_camera (WebCamera)
 echo "SUBSYSTEMS==\"usb\", ENV{DEVTYPE}==\"usb_device\", ATTRS{idVendor}==\"0458\", ATTRS{idProduct}==\"708c\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/99-uvc.rules
 sudo udevadm control --reload-rules
 sudo udevadm trigger
+
 
 # USB Reload
 sudo /etc/init.d/udev reload
