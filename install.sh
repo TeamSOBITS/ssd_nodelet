d7e412044d9e4df8200377409938bcd7c261426b
MrKeith99 <kvalentincardenas@hotmail.com>
2024-08-29 12:08:55 +0900

    [UPD] installer

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-echo "╔══╣ Install: ssd_nodelet (STARTING) ╠══╗"
+echo "╔══╣ Install: ssd_ros (STARTING) ╠══╗"
 
 
-# Download SOBITS Msgs
+# Keep track of the current directory
 DIR=$(pwd)
+
+# Clone required packages
 cd ..
-git clone https://github.com/TeamSOBITS/sobits_msgs.git
-cd $DIR
+git clone -b feature/humble-devel https://github.com/TeamSOBITS/sobits_msgs.git
+git clone -b feature/humble-devel https://github.com/TeamSOBITS/bbox_to_tf.git
+cd bbox_to_tf/
+bash install.sh
+
+# Install Camera Packages
+sudo apt-get install -y \
+    v4l-utils \
+    libboost-python-dev
 
 
 # Install ROS Packages
@@ -24,30 +33,18 @@ sudo apt-get install -y \
     ros-${ROS_DISTRO}-sensor-msgs \
     ros-${ROS_DISTRO}-std-msgs \
     ros-${ROS_DISTRO}-pluginlib \
-    ros-${ROS_DISTRO}-nodelet
-
-
-# Install Camera Packages
-sudo apt-get install -y \
-    v4l-utils 
+    ros-${ROS_DISTRO}-sensor-msgs \
+    ros-${ROS_DISTRO}-std-msgs
 
 sudo apt-get install -y \
-    ros-${ROS_DISTRO}-libuvc-camera \
+    ros-${ROS_DISTRO}-v4l2-camera \
     ros-${ROS_DISTRO}-camera-calibration \
+    ros-${ROS_DISTRO}-image-transport \
+    ros-${ROS_DISTRO}-image-common \
     ros-${ROS_DISTRO}-image-proc
 
 
-# Setting uvc_camera (WebCamera)
-echo "SUBSYSTEMS==\"usb\", ENV{DEVTYPE}==\"usb_device\", ATTRS{idVendor}==\"0458\", ATTRS{idProduct}==\"708c\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/99-uvc.rules
-sudo udevadm control --reload-rules
-sudo udevadm trigger
+cd $DIR
 
 
-# USB Reload
-sudo /etc/init.d/udev reload
-
-v4l2-ctl --list-devices
-v4l2-ctl --list-formats-ext
-
-
-echo "╚══╣ Install: ssd_nodelet (FINISHED) ╠══╝"
+echo "╚══╣ Install: ssd_ros (FINISHED) ╠══╝"
